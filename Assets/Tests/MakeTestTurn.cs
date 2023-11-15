739321d3a7373939d6eb0ffb4baaa4a755aef262
Krupoder <slava.krupoder95@gmail.com>
2023-11-15 10:52:41 +0200

    removed comment

--- Assets/Tests/MakeTestTurn.cs
+++ Assets/Tests/MakeTestTurn.cs
@@ -12,61 +12,56 @@ public class MakeTestTurn
     [Test]
     public void MakeTestTurnSimplePasses()
     {
-        var turnUpdater = new GameObject().AddComponent<TurnUpdater>();
+        var _turnUpdater = new GameObject().AddComponent<TurnUpdater>();
 
-        var unitPrefab1 = Resources.Load<GameObject>("Prefabs/Hero");
-        var unitPrefab2 = Resources.Load<GameObject>("Prefabs/Enemy");
-        var tilePrefab = Resources.Load<GameObject>("Prefabs/SquareTile");
+        var _unitPrefab1 = Resources.Load<GameObject>("Prefabs/Hero");
+        var _unitPrefab2 = Resources.Load<GameObject>("Prefabs/Enemy");
+        var _tilePrefab = Resources.Load<GameObject>("Prefabs/SquareTile");
 
-        var unit1 = Object.Instantiate(unitPrefab1);
-        var unit2 = Object.Instantiate(unitPrefab2);
-        var tile1 = Object.Instantiate(tilePrefab);
-        var tile2 = Object.Instantiate(tilePrefab);
+        var _unit1 = Object.Instantiate(_unitPrefab1);
+        var _unit2 = Object.Instantiate(_unitPrefab2);
+        var _tile1 = Object.Instantiate(_tilePrefab);
+        var _tile2 = Object.Instantiate(_tilePrefab);
 
-        unit1.transform.SetParent(tile1.transform);
-        unit2.transform.SetParent(tile2.transform);
+        _unit1.transform.SetParent(_tile1.transform);
+        _unit2.transform.SetParent(_tile2.transform);
 
-        var unitsOfTurnList = new GameObject[] { unit1, unit2 };
-        var initialHealth = 10;
-        var initialDamage = 5;
+        var _unitsOfTurnList = new GameObject[] { _unit1, _unit2 };
+        var _initialHealth = 10;
+        var _initialDamage = 5;
 
-        var character1 = new TurnUpdater.Character(initialHealth, initialDamage, 0, 0, true, false);
-        var character2 = new TurnUpdater.Character(initialHealth, initialDamage, 1, 1, true, false);
-        
-        var tileData1 = tile1.GetComponent<TileData>();
-        var tileDataInfo1 = new TileData.TileDataInfo
+        var _tileData1 = _tile1.GetComponent<TileData>();
+        var _tileDataInfo1 = new TileData.TileDataInfo
         {
             TileColumn = 0,
             TileRow = 0,
             IsHeroPlace = true,
             IsEnemyPlace = false
         };
-        tileData1.SetDataInfo(tileDataInfo1);
-        var tileData2 = tile2.GetComponent<TileData>();
-        var tileDataInfo2 = new TileData.TileDataInfo
+        _tileData1.SetDataInfo(_tileDataInfo1);
+        var _tileData2 = _tile2.GetComponent<TileData>();
+        var _tileDataInfo2 = new TileData.TileDataInfo
         {
             TileColumn = 1,
             TileRow = 1,
             IsHeroPlace = false,
             IsEnemyPlace = true
         };
-        tileData2.SetDataInfo(tileDataInfo2);
+        _tileData2.SetDataInfo(_tileDataInfo2);
         
-        unitsOfTurnList[heroIndex].GetComponent<UnitsLogic>().SetUnitsParams(initialHealth, initialDamage, true, false);
-        unitsOfTurnList[enemyIndex].GetComponent<UnitsLogic>().SetUnitsParams(initialHealth, initialDamage, true, false);
-        unitsOfTurnList[heroIndex].GetComponentInParent<TileData>().SetDataInfo(tileDataInfo1);
-        unitsOfTurnList[enemyIndex].GetComponentInParent<TileData>().SetDataInfo(tileDataInfo2);
+        _unitsOfTurnList[heroIndex].GetComponent<UnitsLogic>().SetUnitsParams(_initialHealth, _initialDamage, true, false);
+        _unitsOfTurnList[enemyIndex].GetComponent<UnitsLogic>().SetUnitsParams(_initialHealth, _initialDamage, true, false);
+        _unitsOfTurnList[heroIndex].GetComponentInParent<TileData>().SetDataInfo(_tileDataInfo1);
+        _unitsOfTurnList[enemyIndex].GetComponentInParent<TileData>().SetDataInfo(_tileDataInfo2);
         // Act
-        turnUpdater.MakeTurn(unitsOfTurnList);
+        _turnUpdater.MakeTurn(_unitsOfTurnList);
 
         // Assert
-        // Перевірте, чи змінилися параметри першого персонажа
-        Assert.AreEqual(initialHealth + turnUpdater.PlayersBoostList[0][heroIndex], unitsOfTurnList[0].GetComponent<UnitsLogic>().GetUnitsParams().PlayerHealth);
-        Assert.AreEqual(initialDamage + turnUpdater.PlayersBoostList[0][enemyIndex], unitsOfTurnList[0].GetComponent<UnitsLogic>().GetUnitsParams().PlayerDamage);
+        Assert.AreEqual(_initialHealth + _turnUpdater.PlayersBoostList[0][heroIndex], _unitsOfTurnList[0].GetComponent<UnitsLogic>().GetUnitsParams().PlayerHealth);
+        Assert.AreEqual(_initialDamage + _turnUpdater.PlayersBoostList[0][enemyIndex], _unitsOfTurnList[0].GetComponent<UnitsLogic>().GetUnitsParams().PlayerDamage);
 
-        // Перевірте, чи змінилися параметри другого персонажа
-        Assert.AreEqual(initialHealth + turnUpdater.PlayersBoostList[1][heroIndex], unitsOfTurnList[1].GetComponent<UnitsLogic>().GetUnitsParams().PlayerHealth);
-        Assert.AreEqual(initialDamage + turnUpdater.PlayersBoostList[1][enemyIndex], unitsOfTurnList[1].GetComponent<UnitsLogic>().GetUnitsParams().PlayerDamage);
+        Assert.AreEqual(_initialHealth + _turnUpdater.PlayersBoostList[1][heroIndex], _unitsOfTurnList[1].GetComponent<UnitsLogic>().GetUnitsParams().PlayerHealth);
+        Assert.AreEqual(_initialDamage + _turnUpdater.PlayersBoostList[1][enemyIndex], _unitsOfTurnList[1].GetComponent<UnitsLogic>().GetUnitsParams().PlayerDamage);
 
     }
 }
